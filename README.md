78285f1bc64d1c4d8850f005816057b514f92b64
R. Christian McDonald <rcmcdonald91@gmail.com>
2021-04-07 10:35:43 -0400

    clean commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# pfSense-pkg-WireGuard
+
+This is an attempt to port the original WireGuard UI bits as implemented by [Netgate](https://www.netgate.com/) in [pfSense 2.5.0](https://github.com/pfsense/pfsense/tree/RELENG_2_5_0) to a package suitable for sideloading and more frequent updating on future releases of pfSense.
+
+XML configuration bits have been moved from `wireguard/tunnel` to `installedpackages/wireguard/tunnel`
+
+## Build
+The build process is similar to that of other FreeBSD and pfSense packages. You will need to set up a FreeBSD build environment and install or build `wireguard` and `wireguard-kmod` on it. Please check the [pfSense package development documentation](https://docs.netgate.com/pfsense/en/latest/development/developing-packages.html#testing-building-individual-packages) for more information.
+
+`wireguard-kmod` requires headers found in the kernel source and header files in `SRC_BASE=/usr/src` . Here is one solution:
+
+for 12.2-RELEASE , amd64
+```bash
+cd /tmp
+fetch ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/12.2-RELEASE/src.txz
+tar -C / -zxvf src.txz
+rm /tmp/src.txz
+```
+
+## Installation
+This package depends on the `wireguard-tools` and `wireguard-kmod` ports for FreeBSD. Download or build these packages for that version of FreeBSD, then manually install them using `pkg` before installing this package.
+
+Look for latest package links of `wireguard-tools` and `wireguard-kmod` in [FreeBSD 12 repository](https://pkg.freebsd.org/FreeBSD:12:amd64/latest/All/). 
+
+NOTE: As of **4/6/2021**, `wireguard-kmod` is not being actively built by FreshPorts. You will probably have to build these packages manually.
+
+## Configuration
+
+https://docs.netgate.com/pfsense/en/latest/vpn/wireguard/index.html
---
+++ files/usr/local/www/wg/status_wireguard.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * status_wireguard.php
+ *
+ * part of pfSense (https://www.pfsense.org)
+ * Copyright (c) 2020 Ascrod, https://github.com/Ascrod/pfSense-pkg-wireguard
+ * Copyright (c) 2021 R. Christian McDonald, rcmcdonald91@gmail.com
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+##|+PRIV
+##|*IDENT=page-vpn-wg-status
+##|*NAME=Status: WireGuard
+##|*DESCR=Allow access to the 'Status: WireGuard' page.
+##|*MATCH=status_wireguard.php*
+##|-PRIV
+
+require_once("guiconfig.inc");
+require_once("/usr/local/pkg/wireguard/wg.inc");
+
+$shortcut_section = "WireGuard";
+$pgtitle = array(gettext("Status"), "WireGuard");
+
+include("head.inc");
+
+?>
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h2 class="panel-title">Connection Status</h2>
+	</div>
+	<div class="panel-body">
+		<dl class="dl-horizontal">
+			<pre><?php echo wg_status(); ?></pre>
+		</dl>
+    </div>
+</div>
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h2 class="panel-title">WireGuard Version</h2>
+	</div>
+	<div class="panel-body">
+		<dl class="dl-horizontal">
+			<pre><?php echo wg_version(); ?></pre>
+		</dl>
+    </div>
+</div>
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h2 class="panel-title">Kernel Module Status</h2>
+	</div>
+	<div class="panel-body">
+		<dl class="dl-horizontal">
+			<pre><?php echo wg_kmod_status(); ?></pre>
+		</dl>
+    </div>
+</div>
+
+<?php include("foot.inc"); ?>
