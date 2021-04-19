9acc25377bee185b6d94ed6ce39b7fe9220881ef
R. Christian McDonald <rcmcdonald91@gmail.com>
2021-04-19 13:32:25 -0400

    Remove lingering blurring code

--- src/files/usr/local/www/wg/vpn_wg_edit.php
+++ src/files/usr/local/www/wg/vpn_wg_edit.php
@@ -479,20 +479,6 @@ events.push(function() {
 	// Eliminate ghost lines in modal
 	$('.form-group').css({"border-bottom-width" : "0"});
 
-	// Eliminates blurred placeholder text
-	$('body').append('<style>::placeholder{color:revert;text-shadow:none;}</style>');
-
-	// Blurs secrets
-	if (wg_config['blur_secrets'] == 'yes') {
-
-		var blur = {"color" : "transparent", "text-shadow" : "0 0 5px rgba(0,0,0,0.5)"}
-
-		$("#privatekey").css(blur);
-
-		$("#presharedkey").css(blur);
-
-	}
-
 	// Return text from peer table cell
 	function tabletext (row, col) {
 		row++; col++;
