c6c4dd4712e48e68a8343f442439f42990ec48d2
Manojav Sridhar <manojav@manojav.com>
2021-04-29 12:55:17 -0400

    add routes

--- src/files/usr/local/www/wg/status_wireguard.php
+++ src/files/usr/local/www/wg/status_wireguard.php
@@ -124,17 +124,48 @@ if (!empty($a_devices)):
 	endforeach;
 ?>
 		</table>
+
+
+
     	</div>
+
+
 </div>
 
-<div class="panel panel-default">
+<div class="panel panel-default ">
 	<div class="panel-heading">
-		<h2 class="panel-title"><?=gettext('Interface Status')?></h2>
+		<h2 class="panel-title"><?=gettext('Interface Status')?>
+		
+
+
 	</div>
-	<div class="panel-body">
-		<dl class="dl-horizontal">
-			<pre><?=htmlspecialchars(wg_interface_status())?></pre>
-		</dl>
+	
+	<div class="table-responsive panel-body">
+		<table class="table table-hover table-striped table-condensed" style="overflow-x: 'visible'"> 
+	<thead>
+				<th><?=gettext("Interface")?></th>
+				<th><?=gettext("IPs")?></th>
+				<th><?=gettext("MTU")?></th>
+				<th><?=gettext("Flags")?></th>
+				<th><?=gettext("Description")?></th>
+			</thead>
+			<tbody>
+<?php
+$a_intefaces = wg_interface_status();
+foreach ($a_intefaces as $key => $a_interface):
+?>
+				<tr>
+					<td><?=htmlspecialchars(($a_interface['interface']))?></td>
+					<td><?=wg_generate_addresses_popup_link($a_interface['ips'],gettext("IPs"),"vpn_wg_tunnels_edit.php?tun=${a_interface['interface']}")?></td>
+					<td><?=htmlspecialchars(($a_interface['mtu']))?></td>
+					<td><?=htmlspecialchars(($a_interface['flags']))?></td>
+					<td><?=htmlspecialchars(($a_interface['descr']))?></td>
+				<tr>
+<?
+endforeach;
+?>
+			</tbody>	
+		</table>
     </div>
 </div>
 
@@ -148,9 +179,64 @@ endif;
 
 <div class="panel panel-default">
 	<div class="panel-heading">
-		<h2 class="panel-title"><?=gettext('Package Versions')?></h2>
+		<h2 class="panel-title"><?=gettext('Routes Status')?>
+			<span class="widget-heading-icon">
+				<a data-toggle="collapse" href="#wg_routes_status">
+					<!--  actual icon is determined in css based on state of body -->
+					<i class="fa fa-plus-circle"></i>
+				</a>
+			</span>
+		</h2>
 	</div>
-	<div class="table-responsive panel-body">
+
+	<div id="wg_routes_status" class="table-responsive panel-body collapse" aria-expanded="false">
+
+			<div id="IPv4_parent" class="panel-heading"><h2 class="panel-title"><?=gettext("IPv4")?></h2></div>
+				<table class="table table-striped table-hover table-condensed sortable-theme-bootstrap" id="IPv4">
+				<thead>
+					<tr>
+						<th><!-- filled by xhr --></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><?=gettext("Gathering data, please wait...")?></td>
+					</tr>
+				</tbody>
+				</table>
+		
+			<div id="IPv6_parent" class="panel-heading"><h2 class="panel-title"><?=gettext("IPv6")?></h2></div>
+				<table class="table table-striped table-hover table-condensed sortable-theme-bootstrap" id="IPv6">
+				<thead>
+					<tr>
+						<th><!-- filled by xhr --></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><?=gettext("Gathering data, please wait...")?></td>
+					</tr>
+				</tbody>
+				</table>
+	</div>
+
+</div>
+
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h2 class="panel-title"><?=gettext('Package Versions')?>
+		<span class="widget-heading-icon">
+				<a data-toggle="collapse" href="#panel_package_status">
+					<!--  actual icon is determined in css based on state of body -->
+					<i class="fa fa-plus-circle"></i>
+				</a>
+			</span>
+		</h2>
+		</h2>
+	</div>
+
+	<div id="panel_package_status" class="table-responsive panel-body collapse" aria-expanded="false">
 		<table class="table table-hover table-striped table-condensed">
 			<thead>
 				<tr>
