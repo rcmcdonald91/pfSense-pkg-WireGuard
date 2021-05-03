f1bc97df574f41b8dd2b1d0dce4164f7575cf40f
R. Christian McDonald <rcmcdonald91@gmail.com>
2021-05-03 07:06:59 -0400

    Fix allowed ips list on peers

--- src/files/usr/local/www/wg/vpn_wg_tunnels.php
+++ src/files/usr/local/www/wg/vpn_wg_tunnels.php
@@ -154,7 +154,7 @@ display_top_tabs($tab_array);
 										<td><?=htmlspecialchars($peer['descr'])?></td>
 										<td><?=htmlspecialchars(substr($peer['publickey'], 0, 16).'...')?></td>
 										<td><?=htmlspecialchars($peer['peeraddresses'])?></td>
-										<td><?=htmlspecialchars($peer['allowedips'])?></td>
+										<td><?=htmlspecialchars($peer['allowedips']['item'])?></td>
 										<td><?=htmlspecialchars(wg_format_endpoint($peer))?></td>
 									</tr>
 <?php
