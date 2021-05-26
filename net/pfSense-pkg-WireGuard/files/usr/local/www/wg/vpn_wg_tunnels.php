494b02bf3524baabc94debe3f1ab67d2374321bb
R. Christian McDonald <rcmcdonald91@gmail.com>
2021-05-26 17:08:24 -0400

    Working on service return codes

--- net/pfSense-pkg-WireGuard/files/usr/local/www/wg/vpn_wg_tunnels.php
+++ net/pfSense-pkg-WireGuard/files/usr/local/www/wg/vpn_wg_tunnels.php
@@ -44,9 +44,10 @@ if ($_POST) {
 
 	if (isset($_POST['apply'])) {
 
+		// We only want to restart the service if it is already running...
 		if (wg_is_service_running()) {
 
-			$resync_ret = wg_resync_all();
+			$service_ret = wg_service_restart();
 
 		}
 
@@ -112,7 +113,7 @@ wg_display_service_warning();
 
 if (isset($_POST['apply'])) {
 
-	print_apply_result_box($resync_ret['ret_code']);
+	print_apply_result_box($service_ret['ret_code']);
 
 }
 
