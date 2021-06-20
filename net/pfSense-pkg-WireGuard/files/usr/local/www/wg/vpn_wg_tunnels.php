61ae9ac4611b123d5fd39fe961e6be91d18fe33f
R. Christian McDonald <rcmcdonald91@gmail.com>
2021-06-20 18:06:33 -0400

    Working on bug

--- net/pfSense-pkg-WireGuard/files/usr/local/www/wg/vpn_wg_tunnels.php
+++ net/pfSense-pkg-WireGuard/files/usr/local/www/wg/vpn_wg_tunnels.php
@@ -113,7 +113,7 @@ if ($_POST) {
 				mark_subsystem_dirty($wgg['subsystems']['wg']);
 
 				// Add tunnel to the list to apply
-				wg_apply_list_add($res['tun_to_sync'], 'tunnels');
+				wg_apply_list_add('tunnels', $res['tun_to_sync']);
 
 			}
 
